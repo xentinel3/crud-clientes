9e48ace3650b52e18ba87073e9b16f896fe7d540
jromero <ing.juliocesar26@outlook.com>
2024-08-07 21:26:07 -0600

    Initial commit

---
+++ src/main/java/mx/com/julio/crud_clientes/CrudClientesApplication.java
@@ -0,0 +1,13 @@
+package mx.com.julio.crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/julio/crud_clientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.julio.crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
